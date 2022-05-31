3b61b1b9158ece11e5d89c3bcb73a74e0006121c
Milan anand raj <84122339+manandraj20@users.noreply.github.com>
2022-06-01 03:31:03 +0530

    Update H.java

--- Episodes/Episode-023/H.java
+++ Episodes/Episode-023/H.java
@@ -9,65 +9,78 @@ public class H
       FastScanner in = new FastScanner(System.in);
       PrintWriter out = new PrintWriter(System.out);
       int T = in.nextInt();
-      for (int tc=1; tc<=T; tc++)
-         new H(tc, in, out);
-      out.close();
+      for (int tc=1; tc<=T; tc++)// no of test cases
+         new H(tc, in, out);// running tarjan algorithm on every test case
+      out.close();//closing the stdout
    }
 
    public H(int tc, FastScanner in, PrintWriter out)
    {
-      Graph g = new Graph(in.nextInt());
-      int M = in.nextInt();
-      while (M-->0)
+      Graph g = new Graph(in.nextInt());//taking the number of nodes in the graph
+      int M = in.nextInt();//taking the number of edges
+      while (M-->0)//scanning every edge
       {
          int i = in.nextInt()-1;
          int j = in.nextInt()-1;
-         g.add(i, j);
+         g.add(i, j);//adding current edge to the graph
       }
 
-      Tarjan scc = new Tarjan(g);
+      Tarjan scc = new Tarjan(g);// calling Tarjan on the graph
 
-      if (scc.numSCCs == 1)
-         out.println(0);
-      else
-      {
-         int[] indeg = new int[scc.numSCCs];
-         int[] outdeg = new int[scc.numSCCs];
+//       if (scc.numSCCs == 1)
+//          out.println(0);
+//       else
+//       {
+//          int[] indeg = new int[scc.numSCCs];
+//          int[] outdeg = new int[scc.numSCCs];
 
-         for (int i=0; i<g.N; i++)
-            for (int j : g.adj[i])
-               if (scc.id[i] != scc.id[j])
-               {
-                  outdeg[scc.id[i]]++;
-                  indeg[scc.id[j]]++;
-               }
+//          for (int i=0; i<g.N; i++)
+//             for (int j : g.adj[i])
+//                if (scc.id[i] != scc.id[j])
+//                {
+//                   outdeg[scc.id[i]]++;
+//                   indeg[scc.id[j]]++;
+//                }
          
-         int numSrcs = 0;
-         int numSnks = 0;
-         for (int i=0; i<scc.numSCCs; i++)
-         {
-            if (indeg[i] == 0)
-               numSrcs++;
-            if (outdeg[i] == 0)
-               numSnks++;
-         }
-         out.println(Math.max(numSrcs, numSnks));
-      }
+//          int numSrcs = 0;
+//          int numSnks = 0;
+//          for (int i=0; i<scc.numSCCs; i++)
+//          {
+//             if (indeg[i] == 0)
+//                numSrcs++;
+//             if (outdeg[i] == 0)
+//                numSnks++;
+//          }
+//          out.println(Math.max(numSrcs, numSnks));
+//       }
 
-      Graph meta = new Graph(scc.numSCCs);
-      for (int i=0; i<g.N; i++)
-         for (int j : g.adj[i])
-            if (scc.id[i] != scc.id[j])
-               meta.add(scc.id[i], scc.id[j]);
+//       Graph meta = new Graph(scc.numSCCs);
+//       for (int i=0; i<g.N; i++)
+//          for (int j : g.adj[i])
+//             if (scc.id[i] != scc.id[j])
+//                meta.add(scc.id[i], scc.id[j]);
+//    }
+      for(int currComp=0;currComp<scc.numSCCs;currComp++){
+         out.printf("%d: ",currComp+1);
+         for(int i=0;i<g.N;i++){
+            if(scc.id[i]==currComp){
+               out.printf("%d ", i+1);
+               
+            }
+         }
+         out.println();
+         
+      }
    }
+      
 }
 
 class Tarjan
 {
    Graph g;
-   int[] pre;
-   int[] id;
-   int[] low;
+   int[] pre;//order of traversing the graph or t_in time
+   int[] id;//id to denote the strongly connected component a node belongs to
+   int[] low;// low link value
    ArrayDeque<Integer> stk;
    int preorderCounter;
    int numSCCs;
@@ -99,20 +112,21 @@ class Tarjan
       {
          if (pre[j] == -1)
             dfs(j);
-      
-         low[i] = Math.min(low[i], low[j]);
+      //Fwd, backward and cross edge case
+         low[i] = Math.min(low[i], low[j]);// taking min with the low link of children ; here we have taken low with low of children even in the case 
+         //of backward edge this is true as we are dealing with bridges and not articulation points here
       }
 
       // Are we root of the SCC?
       if (low[i] == pre[i])
-      {
+      {System.out.println(stk);
          while (true)
          {
             int j = stk.pop();
             id[j] = numSCCs;
-            low[j] = g.N;
+            low[j] = g.N;//setting the low link value high enough that cross edges update is eliminated
          
-            if (i == j)
+            if (i == j)// when you hit the node which started the scc then break
                break;
          }
          numSCCs++;
